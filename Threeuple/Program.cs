f9951bef7c339a94e13c67c85b0aeac491971453
Jorjo77 <61197544+Jorjo77@users.noreply.github.com>
2020-10-29 17:22:38 +0200

    TasksUpdate

---
+++ Threeuple/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine()
+                .Split();
+            string personName = firstInput[0] + " " + firstInput[1];
+            string addres = firstInput[2];
+            string town = "";
+            if (firstInput.Length==4)
+            {
+                town = firstInput[3];
+            }
+            if (firstInput.Length == 5)
+            {
+                town = firstInput[3]+ " " +firstInput[4];
+            }
+
+            Threeuple<string, string, string> personInformation = new Threeuple<string, string, string>(personName, addres, town);
+
+            string[] secondInput = Console.ReadLine()
+                .Split();
+            string name = secondInput[0];
+            string litersOfBeer = secondInput[1];
+            string drunkOrNot = secondInput[2];
+            bool IsDrink = true;
+            if (drunkOrNot == "drunk")
+            {
+                IsDrink = true;
+            }
+            else if (drunkOrNot == "not")
+            {
+                IsDrink = false;
+            }
+
+            Threeuple<string, string, bool> personDrinkInformation = new Threeuple<string, string, bool>(name, litersOfBeer, IsDrink);
+
+            string[] thirdInput = Console.ReadLine()
+                .Split();
+            string clientName = thirdInput[0];
+            double accountBallance = double.Parse(thirdInput[1]);
+            string bankName = thirdInput[2];
+            Threeuple<string, double, string> personBankInformation = new Threeuple<string, double, string>(clientName, accountBallance, bankName);
+
+            Console.WriteLine(personInformation);
+            Console.WriteLine(personDrinkInformation);
+            Console.WriteLine(personBankInformation);
+        }
+    }
+}
---
+++ Tuple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine()
+                .Split();
+            string personName = firstInput[0] + " " + firstInput[1];
+            string adress = firstInput[2];
+            Tuple<string, string> personInfo = new Tuple<string, string>(personName, adress);
+
+            string[] secondInput = Console.ReadLine()
+                .Split();
+
+            string name = secondInput[0];
+            int beerLiters = int.Parse(secondInput[1]);
+            Tuple<string, int> personBeerInfo = new Tuple<string, int>(name, beerLiters);
+
+            string[] thirdInput = Console.ReadLine()
+                .Split();
+            int fitstNumber = int.Parse(thirdInput[0]);
+            double secondNumber = double.Parse(thirdInput[1]);
+
+            Tuple<int, double> numbersInfo = new Tuple<int, double>(fitstNumber, secondNumber);
+
+            Console.WriteLine(personInfo);
+            Console.WriteLine(personBeerInfo);
+            Console.WriteLine(numbersInfo);
+
+        }
+    }
+}
